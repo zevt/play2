ba43388125e7884e235deacf3b71e45b13c9f909
Viet Quoc Tran <viet.tran@siemens.com>
2019-01-24 18:15:02 -0600

    init

---
+++ app/controllers/ApiController.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.Play
+import play.api.mvc.{AbstractController, Controller, ControllerComponents, Result}
+
+
+
+//class ApiController extends Controller {
+//  def index: Result = {
+//    val applicationName = Play.application().config().getString("application.name")
+//    Ok(applicationName)
+//  }
+//}
